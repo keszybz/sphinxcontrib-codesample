04da77b3e30d8255a9d12bf1725aa617d5591b4b
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-03-24 11:35:56 -0400

    tests: add basic sanity check

---
+++ sphinxcontrib/test/test_codesample_directive.py
@@ -0,0 +1,32 @@
+examples = ('''\
+x = 2
+y = x**2
+y += 1                 # SUPPRESS
+print(y)
+''',
+
+'''\
+def f(a=3):
+    return a ** 3
+
+print(f)
+print(f(3))
+''')
+
+from sphinxcontrib.codesample import CodesampleDirective
+
+import pytest
+
+@pytest.mark.parametrize("block", examples)
+@pytest.mark.parametrize("suppress", (False, True, None))
+def test_basic(block, suppress):
+    options = {}
+    if suppress is not None:
+        options['suppress'] = suppress
+
+    content = block.split('\n')
+
+    CodesampleDirective('debug', arguments=None, options=options,
+                          content=content, lineno=0,
+                          content_offset=None, block_text=None,
+                          state=None, state_machine=None)
